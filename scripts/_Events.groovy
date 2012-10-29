0102c8e75f2c553ebf458897b76768906da8d83c
Karol Balejko <kb@groovydev.com>
2012-10-29 20:13:31 +0100

    cleanup

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -2,9 +2,6 @@ includeTargets << new File("${modulesManagerPluginDir}/scripts/_RefreshModules.g
 
 eventCompileStart = {
 
-    println "grailsSettings.config.modules?.refreshOnCompile ${grailsSettings.config.modules?.refreshOnCompile}"
-    println "grailsSettings.config.modules?.forceOverride ${grailsSettings.config.modules?.forceOverride}"
-
     if (grailsSettings.config.modules?.refreshOnCompile) {
         refreshModules(grailsSettings.config.modules?.forceOverride)
     }
